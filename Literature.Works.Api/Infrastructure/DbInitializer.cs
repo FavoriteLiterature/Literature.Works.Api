84158d6b9843833b0466443b569ebd77d4302d5f
Tsypin Ilya <tsypin.i.p@mail.ru>
2023-01-16 20:50:02 +0300

    Add Db initializer

---
+++ Literature.Works.Api/Infrastructure/DbInitializer.cs
@@ -0,0 +1,25 @@
+﻿using Literature.Works.Api.Entities;
+
+namespace Literature.Works.Api.Infrastructure;
+
+public static class DbInitializer
+{
+    public static async Task Initialize(DataContext context)
+    {
+        if (!context.DbGenres.Any())
+        {
+            await context.AddAsync(new Genre {Name = "Экшн"});
+            await context.AddAsync(new Genre {Name = "Фантастика"});
+            await context.AddAsync(new Genre {Name = "Фанфики"});
+        }
+        
+        if (!context.DbAttachmentTypes.Any())
+        {
+            await context.AddAsync(new AttachmentType {Name = "Cover"});
+            await context.AddAsync(new AttachmentType {Name = "Image"});
+            await context.AddAsync(new AttachmentType {Name = "Document"});
+        }
+        
+        await context.SaveChangesAsync();
+    }
+}
--- Literature.Works.Api/Program.cs
+++ Literature.Works.Api/Program.cs
@@ -1,10 +1,33 @@
+using Literature.Works.Api.Infrastructure;
+
 namespace Literature.Works.Api;
 
 public class Program
 {
-    public static async Task Main(string[] args) =>
-        await CreateHostBuilder(args).Build().RunAsync();
+    public static async Task Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        await CreateDbIfNotExists(host);
+        await host.RunAsync();
+    }
 
+    private static async Task CreateDbIfNotExists(IHost host)
+    {
+        using var scope = host.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        
+        try
+        {
+            var context = services.GetRequiredService<DataContext>();
+            await DbInitializer.Initialize(context);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred creating the DB.");
+        }
+    }
+    
     private static IHostBuilder CreateHostBuilder(string[] args) =>
         Host
             .CreateDefaultBuilder(args)
